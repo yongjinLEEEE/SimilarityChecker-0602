dda46e6c22760c190af15bc4422130680bc421bf
yongjin lee <vir2048@gmail.com>
2023-06-02 13:34:45 +0900

    add Partial calc
    unit test case
    AAABB, BAA
    test result - pass
    
    Signed-off-by: yongjin lee <vir2048@gmail.com>

--- Project10/SimilarityChecker.cpp
+++ Project10/SimilarityChecker.cpp
@@ -4,6 +4,27 @@
 
 int SimilarityChecker::checkstrlength(const vector<string>& input)
 {
-	if (input[0] == "ASD" && input[1] == "DSA")
+	if (input[0].size() == input[1].size())
+	{
 		return 60;
+	} else
+	{
+		if (input[0].size() > input[1].size()) {
+			int tmp_size;
+			tmp_size = input[0].size() - input[1].size();
+			if (tmp_size >= input[1].size() * 2)
+				return 0;
+			return (1 - (double)tmp_size / input[1].size()) * 60 + 0.5;
+
+		} else {
+			int tmp_size;
+			tmp_size = input[1].size() - input[0].size();
+			if (tmp_size >= input[0].size() * 2)
+				return 0;
+
+			return (1 - (double)tmp_size / input[0].size()) * 60 + 0.5;
+		}
+	}
+
+	return 0;
 }
