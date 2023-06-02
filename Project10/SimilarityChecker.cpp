0049f97b729df7d0aaa3b75c741e4d976ba6a297
yongjin lee <vir2048@gmail.com>
2023-06-02 11:49:41 +0900

    Add checkstrlength()
    Signed-off-by: yongjin lee <vir2048@gmail.com>

---
+++ Project10/SimilarityChecker.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+#include "SimilarityChecker.h"
+
+int SimilarityChecker::checkstrlength(const vector<string>& input)
+{
+	if (input[0] == "ASD" && input[1] == "DSA")
+		return 60;
+}
--- Project10/SimilarityChecker.h
+++ Project10/SimilarityChecker.h
@@ -1,5 +1,7 @@
 #pragma once
+
+using namespace std;
 class SimilarityChecker {
 public:
-
+	int checkstrlength(const vector<string>& input);
 };
