98220e6e954720be467c96efc69b7ff19fcad4a4
yongjin lee <vir2048@gmail.com>
2023-06-02 14:10:06 +0900

    [checker] refactoring for length score
    test result - pass
    
    Signed-off-by: yongjin lee <vir2048@gmail.com>

--- Project10/SimilarityChecker.cpp
+++ Project10/SimilarityChecker.cpp
@@ -2,29 +2,42 @@
 #include <vector>
 #include "SimilarityChecker.h"
 
-int SimilarityChecker::checkstrlength(const vector<string>& input)
+int SimilarityChecker::partialCalc(vector<string>& input)
+{
+	return (1 - (double)(input[0].size() - input[1].size()) / input[1].size()) * 60 + 0.5;
+}
+
+void SimilarityChecker::sort(vector<string>& input)
+{
+	if (input[0].size() < input[1].size())
+	{
+		string str_tmp;
+		str_tmp = input[1];
+		input[1] = input[0];
+		input[0] = str_tmp;
+	}
+}
+
+bool SimilarityChecker::checkMaxCondition(vector<string>& input)
 {
 	if (input[0].size() == input[1].size())
-	{
-		return 60;
-	} else
-	{
-		if (input[0].size() > input[1].size()) {
-			int tmp_size;
-			tmp_size = input[0].size() - input[1].size();
-			if (tmp_size >= input[1].size() * 2)
-				return 0;
-			return (1 - (double)tmp_size / input[1].size()) * 60 + 0.5;
+		return true;
+	return false;
+}
 
-		} else {
-			int tmp_size;
-			tmp_size = input[1].size() - input[0].size();
-			if (tmp_size >= input[0].size() * 2)
-				return 0;
+bool SimilarityChecker::check0Condition(vector<string>& input)
+{
+	if (input[0].size() - input[1].size() >= input[1].size() * 2)
+		return true;
+	return false;
+}
 
-			return (1 - (double)tmp_size / input[0].size()) * 60 + 0.5;
-		}
-	}
+int SimilarityChecker::checkstrlength(vector<string>& input) {
+	if (checkMaxCondition(input)) return 60;
 
-	return 0;
+	/* 문자열 길이 기준으로 내림차순 정렬 */
+	SimilarityChecker().sort(input);
+
+	if (check0Condition(input)) return 0;
+	return SimilarityChecker().partialCalc(input);
 }
--- Project10/SimilarityChecker.h
+++ Project10/SimilarityChecker.h
@@ -3,5 +3,11 @@
 using namespace std;
 class SimilarityChecker {
 public:
-	int checkstrlength(const vector<string>& input);
+	int checkstrlength(vector<string>& input);
+
+private:
+	void sort(vector<string>& input);
+	bool checkMaxCondition(vector<string>& input);
+	bool check0Condition(vector<string>& input);
+	int partialCalc(vector<string>& input);
 };
