09d136a1a3a8c475cdca970dd9041c28a5fb2da5
yongjin lee <vir2048@gmail.com>
2023-06-02 11:53:02 +0900

    Add unit test case
    nput data - A, BB
    test result: ok
    
    Signed-off-by: yongjin lee <vir2048@gmail.com>

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,12 @@
 #include "pch.h"
 #include "../Project10/SimilarityChecker.cpp"
 
-TEST(TestCaseName, checkStrLength) {
+TEST(TestCaseName, checkStrLength0) {
 	vector <string> input = { "ASD", "DSA" };
 	EXPECT_EQ(60, SimilarityChecker().checkstrlength(input));
-  
+}
+
+TEST(TestCaseName, checkStrLength1) {
+	vector <string> input = { "A", "BB" };
+	EXPECT_EQ(0, SimilarityChecker().checkstrlength(input));
 }
